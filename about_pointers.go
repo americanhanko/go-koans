8106d68a28ed47d14e35294a40a5fcd645c244cc
Eric Hanko <eric.hanko@oracle.com>
2020-04-25 19:59:15 -0700

    finish about_pointers.go

--- about_pointers.go
+++ about_pointers.go
@@ -7,15 +7,33 @@ func aboutPointers() {
 
 		b++
 
-		assert(a == __int__) // variables are independent of one another
+		assert(a == 3) // variables are independent of one another
 	}
 
 	{
 		a := 3
-		b := &a // 'b' is the address of 'a'
 
-		*b = *b + 2          // de-referencing 'b' means acting like a mutable copy of 'a'
-		assert(a == __int__) // pointers seem complicated at first but are actually simple
+		// 'b' is the address of 'a'
+		// '&' returns the memory address of 'a'
+		// in other words, this is _not_ a copy operation, and we are able to mutate
+		// 'a' and 'b' are references to the same memory address
+		b := &a
+
+		// de-referencing 'b' means acting like a mutable copy of 'a'
+		// since we assigned 'b' to the memory address of 'a'
+		// if we want to change the value
+		// we must de-reference 'b' to return an int
+		*b = *b + 2
+
+		/*
+			println(&a) ... memory address -> 0xc00005fed8
+			println(b)  ... also memory address -> 0xc00005fed8
+			println(a)  ... un-references value -> 5
+			println(*b) ... de-references value -> 5
+			println(&b) ... still a memory address -> 0xc00005fed8
+		*/
+
+		assert(a == 5) // pointers seem complicated at first but are actually simple
 	}
 
 	{
@@ -25,7 +43,7 @@ func aboutPointers() {
 
 		a := 3
 		increment(a)
-		assert(a == __int__) // variables are always passed by value, and so a copy is made
+		assert(a == 3) // variables are always passed by value, and so a copy is made
 	}
 
 	{
@@ -35,6 +53,6 @@ func aboutPointers() {
 
 		b := 3
 		realIncrement(&b)
-		assert(b == __int__) // but passing a pointer allows others to mutate the value pointed to
+		assert(b == 4) // but passing a pointer allows others to mutate the value pointed to
 	}
 }
